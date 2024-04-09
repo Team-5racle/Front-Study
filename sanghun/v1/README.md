dadf811e4963593bc255594777d3a64bb159769f
Sanghun Kim <rlatkdgns042@naver.com>
2024-04-09 14:22:13 +0900

    new file:   sanghun/v1/README.md 	new file:   sanghun/v1/assets/asset.png 	new file:   sanghun/v1/data/data.json 	new file:   sanghun/v1/img/blue_p.png 	new file:   sanghun/v1/img/blue_s.png 	new file:   sanghun/v1/img/blue_t.png 	new file:   sanghun/v1/img/logo.png 	new file:   sanghun/v1/img/pink_p.png 	new file:   sanghun/v1/img/pink_s.png 	new file:   sanghun/v1/img/pink_t.png 	new file:   sanghun/v1/img/yellow_p.png 	new file:   sanghun/v1/img/yellow_s.png 	new file:   sanghun/v1/img/yellow_t.png 	new file:   sanghun/v1/index.html 	new file:   sanghun/v1/src/main.js 	new file:   sanghun/v1/style.css 	new file:   sanghun/v2/text

---
+++ sanghun/v1/README.md
@@ -0,0 +1,3 @@
+### preview
+
+<img src="https://github.com/rlatkd/online-shopping-v1/blob/main/assets/asset.png">
---
+++ sanghun/v1/src/main.js
@@ -0,0 +1,51 @@
+// JSON에서 아이템 데이터 가져옴
+function loadItems() {
+    return fetch('data/data.json')
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+// 가져온 데이터를 HTML으로 만듬
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+        `;
+}
+
+// 만들어진 HTML 문자열로 리스트 업데이트
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// 버튼클릭 이벤트 핸들러
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+
+    displayItems(items.filter(item => item[key] === value));
+}
+
+// 이벤트 리스너 세팅
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+// main
+loadItems()
+    .then(items => {
+        displayItems(items);
+        setEventListeners(items);
+    })
+    .catch(console.log);
